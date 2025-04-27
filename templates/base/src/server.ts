cb3463d90ad54d54e5bcc42c2871d0cc35d1fb47
Brandon Korous <me@brandonkorous.com>
2025-04-27 04:15:28 -0700

    feat: add DB_NAME environment variable and enhance logging functionality

--- templates/base/src/server.ts
+++ templates/base/src/server.ts
@@ -5,24 +5,36 @@ import './config/dotenv';
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import { createYoga } from 'graphql-yoga';
-import { createServerSchema, createServerContext, registerSchemaRoutes } from '@wizeworks/graphql-factory-mongo';
+import { createServerSchema, createServerContext, registerSchemaRoutes, registerAdminRoutes } from '@wizeworks/graphql-factory-mongo';
+import { logger } from './config/logger';
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-const mongodUri = process.env.MONGO_URI || 'mongodb://localhost:27017/';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
 const database = process.env.DB_NAME || 'wize-example';
-const mongoClient = new MongoClient(mongodUri);
+const mongoClient = new MongoClient(MONGO_URI);
+let currentSchemas: any = null;
+
+
 
 const start = async () => {
     await mongoClient.connect();
+    logger.info('MongoDB connected');
+    logger.info(`Using database: ${database}`);
 
     const yoga = createYoga({
         graphqlEndpoint: '/graphql',
-        schema: (args) => createServerSchema(args.request, mongoClient, database),
+        schema: async ({request}) => {
+            if(!currentSchemas) {
+                const apiKey: string = request.headers.get('wize-api-key') || '';
+                currentSchemas = await createServerSchema(apiKey, mongoClient, database);
+            }
+            return currentSchemas;
+        },
         context: async ({request}) => {
             const baseContext = await createServerContext(request, mongoClient);
             return {
                 ...baseContext,
-                dbName
+                database,
             };
         },
         graphiql: true
@@ -31,14 +43,14 @@ const start = async () => {
     const app = express();
     app.use(express.json());
     
-    const schema = registerSchemaRoutes(app, mongoClient, database);
-
+    registerSchemaRoutes(app, mongoClient, database);
+    registerAdminRoutes(app, mongoClient, currentSchemas, database);
 
     // Use Yoga as middleware in Express
     app.use(yoga.graphqlEndpoint, yoga);
 
     app.listen(port, () => {
-        console.log(`🚀 __PROJECT_NAME__ API ready at http://localhost:${port}/graphql`);
+        console.log(`🚀 wize-example API ready at http://localhost:${port}/graphql`);
     });
 };
 
